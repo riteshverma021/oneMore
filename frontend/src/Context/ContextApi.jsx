f94cf028b1e580907ab0a7c37b93f82bffccffb0
Your Name <you@example.com>
2025-02-02 00:03:18 +0530

    first-commit

--- frontend/src/Context/ContextApi.jsx
+++ frontend/src/Context/ContextApi.jsx
@@ -2,10 +2,10 @@ import React, { createContext, useContext } from 'react';
 
 // Create the context
 export const BaseURLContext = createContext();
-
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
 // Provider component to wrap the app
 export const BaseURLProvider = ({ children }) => {
-    const BASE_URL = "http://localhost:1002"; // Replace with your actual base URL
+    const BASE_URL = backendUrl; // Replace with your actual base URL
 
     return (
         <BaseURLContext.Provider value={BASE_URL}>
